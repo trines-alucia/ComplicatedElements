eec85a0e95299c3c8617fa39f336b298c49a62cf
alucia <157240401+trines-alucia@users.noreply.github.com>
2025-10-08 15:23:50 +0200

    intial commit
    finally made a github repository! this is the initial commit so yeah

---
+++ ComplicatedElements.cs
@@ -0,0 +1,28 @@
+using Brimstone;
+using Quintessential;
+
+namespace ComplicatedElements;
+
+public class ComplicatedElements : QuintessentialMod
+{
+    public override void PostLoad()
+    {
+        //ignore
+    }
+    public override void Load()
+    {
+        Quintessential.Logger.Log("ComplicatedElements: Registered");
+    }
+    public override void Unload()
+    {
+        Quintessential.Logger.Log("ComplicatedElements: Unloaded");
+    }
+
+    public override void LoadPuzzleContent()
+    {
+        Quintessential.Logger.Log("ComplicatedElements: Loading!");
+        ComplicatedElementsAtoms.AddAtomTypes();
+        //QApi.AddPuzzlePermission(CrystalPermission, "Glyph of Crystallization", "Complicated Elements");
+        Quintessential.Logger.Log("Loading complete, yay!");
+    }
+}
---
+++ ComplicatedElementsAtoms.cs
@@ -0,0 +1,46 @@
+﻿using Brimstone;
+using Quintessential;
+
+namespace ComplicatedElements;
+
+public static class ComplicatedElementsAtoms
+{
+    public static AtomType Amethyst, Citrine, Azurine;
+
+    public static void AddAtomTypes()
+    {
+        Amethyst = Brimstone.API.CreateCardinalAtom(
+            ID: 150, 
+            modName: "ComplicatedElements",
+            name: "Amethyst",
+            pathToSymbol: "textures/atoms/alucia/ComplicatedElements/cardinals/amethyst_symbol",
+            pathToBase: "textures/atoms/alucia/ComplicatedElements/cardinals/amethyst_base",
+            pathToFog: "textures/atoms/alucia/ComplicatedElements/cardinals/amethyst_fog",
+            pathToRim: "textures/atoms/alucia/ComplicatedElements/cardinals/atom_rim",
+            pathToShadow: "textures/atoms/alucia/ComplicatedElements/cardinals/amethyst_shadow"
+        );
+        Citrine = Brimstone.API.CreateCardinalAtom(
+            ID: 151, 
+            modName: "ComplicatedElements",
+            name: "Citrine",
+            pathToSymbol: "textures/atoms/alucia/ComplicatedElements/cardinals/citrine_symbol",
+            pathToBase: "textures/atoms/alucia/ComplicatedElements/cardinals/citrine_base",
+            pathToFog: "textures/atoms/alucia/ComplicatedElements/cardinals/nam_atom_fog",
+            pathToRim: "textures/atoms/alucia/ComplicatedElements/cardinals/atom_rim",
+            pathToShadow: "textures/atoms/alucia/ComplicatedElements/cardinals/citrine_shadow"
+        );
+        Azurine = Brimstone.API.CreateCardinalAtom(
+            ID: 152, 
+            modName: "ComplicatedElements",
+            name: "Azurine",
+            pathToSymbol: "textures/atoms/alucia/ComplicatedElements/cardinals/azurine_symbol",
+            pathToBase: "textures/atoms/alucia/ComplicatedElements/cardinals/azurine_base",
+            pathToFog: "textures/atoms/alucia/ComplicatedElements/cardinals/nam_atom_fog",
+            pathToRim: "textures/atoms/alucia/ComplicatedElements/cardinals/atom_rim",
+            pathToShadow: "textures/atoms/alucia/ComplicatedElements/cardinals/azurine_shadow"
+        );
+        QApi.AddAtomType(Amethyst);
+        QApi.AddAtomType(Citrine);
+        QApi.AddAtomType(Azurine);
+    }
+}
