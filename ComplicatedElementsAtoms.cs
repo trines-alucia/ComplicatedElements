d7813ed992914017f59ff89e04c2f2d5ce275129
alucia <157240401+trines-alucia@users.noreply.github.com>
2025-12-19 21:34:19 +0100

    Changed the atoms slightly to not look terrible
    Title. They still do, but not as much as before.

--- ComplicatedElementsAtoms.cs
+++ ComplicatedElementsAtoms.cs
@@ -14,42 +14,48 @@ public static class ComplicatedElementsAtoms
             modName: "ComplicatedElements",
             name: "Vaprorine",
             pathToSymbol: "textures/atoms/alucia/ComplicatedElements/Normals/Vaprorine_symbol",
-            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Vaprorine_diffuse"
+            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Vaprorine_diffuse",
+            pathToShade: "textures/atoms/alucia/ComplicatedElements/Normals/mors_shade"
         );
         Pyrolite = Brimstone.API.CreateNormalAtom(
             ID: 151, 
             modName: "ComplicatedElements",
             name: "Pyrolite",
             pathToSymbol: "textures/atoms/alucia/ComplicatedElements/Normals/Pyrolite_symbol",
-            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Pyrolite_diffuse"
+            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Pyrolite_diffuse",
+            pathToShade: "textures/atoms/alucia/ComplicatedElements/Normals/mors_shade"
         );
         Aerolith = Brimstone.API.CreateNormalAtom(
             ID: 152, 
             modName: "ComplicatedElements",
             name: "Aerolith",
             pathToSymbol: "textures/atoms/alucia/ComplicatedElements/Normals/Aerolith_symbol",
-            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Aerolith_diffuse"
+            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Aerolith_diffuse",
+            pathToShade: "textures/atoms/alucia/ComplicatedElements/Normals/mors_shade"
         );
         Mistaline = Brimstone.API.CreateNormalAtom(
             ID: 153, 
             modName: "ComplicatedElements",
             name: "Mistaline",
             pathToSymbol: "textures/atoms/alucia/ComplicatedElements/Normals/Mistaline_symbol",
-            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Mistaline_diffuse"
+            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Mistaline_diffuse",
+            pathToShade: "textures/atoms/alucia/ComplicatedElements/Normals/mors_shade"
         );
         Ignistal = Brimstone.API.CreateNormalAtom(
             ID: 154, 
             modName: "ComplicatedElements",
             name: "Ignistal",
             pathToSymbol: "textures/atoms/alucia/ComplicatedElements/Normals/Ignistal_symbol",
-            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Ignistal_diffuse"
+            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Ignistal_diffuse",
+            pathToShade: "textures/atoms/alucia/ComplicatedElements/Normals/mors_shade"
         );
         Terramarine = Brimstone.API.CreateNormalAtom(
             ID: 155, 
             modName: "ComplicatedElements",
             name: "Terramarine",
             pathToSymbol: "textures/atoms/alucia/ComplicatedElements/Normals/Terramarine_symbol",
-            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Terramarine_diffuse"
+            pathToDiffuse: "textures/atoms/alucia/ComplicatedElements/Normals/Terramarine_diffuse",
+            pathToShade: "textures/atoms/alucia/ComplicatedElements/Normals/mors_shade"
         );
         Quicklime = Brimstone.API.CreateNormalAtom(
             ID: 132, 
